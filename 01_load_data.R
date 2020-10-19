ca45648b14f516c80fc8cab7605570eaa18281d1
Kessler <ak113714@bna.com>
2020-10-19 10:40:58 -0400

    initial commit

---
+++ 01_load_data.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(janitor)
+library(lubridate)
+
+test_state <- read_tsv("raw_data/20201019_1_avev_delivery_state.txt.gz", col_types = cols(.default = "c"))
+test_county <- read_tsv("raw_data/20201019_1_avev_delivery_county.txt.gz", col_types = cols(.default = "c"))
+
+
+state_2016 <- read_tsv("raw_data/2016_avev_delivery_state.txt.gz", col_types = cols(.default = "c"))
+county_2016 <- read_tsv("raw_data/2016_avev_delivery_county.txt.gz", col_types = cols(.default = "c"))
