83029538e0fc930c733c34ef9bca2cfccb2af045
Kessler <ak113714@bna.com>
2020-10-19 17:23:02 -0400

    format 2020 data columns

--- 01_load_data.R
+++ 01_load_data.R
@@ -2,13 +2,31 @@ library(tidyverse)
 library(janitor)
 library(lubridate)
 
-test_state <- read_tsv("raw_data/20201019_1_avev_delivery_state.txt.gz", col_types = cols(.default = "c"))
-test_county <- read_tsv("raw_data/20201019_1_avev_delivery_county.txt.gz", col_types = cols(.default = "c"))
+#import raw daily files for 2020
+state_latest <- read_tsv("raw_data/20201019_1_avev_delivery_state.txt.gz", col_types = cols(.default = "c"))
+county_latest <- read_tsv("raw_data/20201019_1_avev_delivery_county.txt.gz", col_types = cols(.default = "c"))
+
+#format columns
+state_latest
+
+state_latest <- state_latest %>% 
+  mutate(
+    days_to_election = as.numeric(days_to_election),
+    ballots_requested = as.numeric(ballots_requested),
+    ballots_returned = as.numeric(ballots_returned)
+  )
+
+county_latest <- county_latest %>% 
+  mutate(
+    days_to_election = as.numeric(days_to_election),
+    ballots_requested = as.numeric(ballots_requested),
+    ballots_returned = as.numeric(ballots_returned)
+  )
 
 
+
+#import raw historical files for 2016
 state_2016 <- read_tsv("raw_data/2016_avev_delivery_state.txt.gz", col_types = cols(.default = "c"))
 county_2016 <- read_tsv("raw_data/2016_avev_delivery_county.txt.gz", col_types = cols(.default = "c"))
 
-test_county %>% 
-  filter(countyfips == "31055") %>% 
-  write_csv("sample.csv")
+
