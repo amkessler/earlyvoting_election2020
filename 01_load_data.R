182d875ec72dfb0fc6f71bb76ebe499302a1168e
Kessler <ak113714@bna.com>
2020-10-19 11:13:30 -0400

    export sample

--- 01_load_data.R
+++ 01_load_data.R
@@ -8,3 +8,7 @@ test_county <- read_tsv("raw_data/20201019_1_avev_delivery_county.txt.gz", col_t
 
 state_2016 <- read_tsv("raw_data/2016_avev_delivery_state.txt.gz", col_types = cols(.default = "c"))
 county_2016 <- read_tsv("raw_data/2016_avev_delivery_county.txt.gz", col_types = cols(.default = "c"))
+
+test_county %>% 
+  filter(countyfips == "31055") %>% 
+  write_csv("sample.csv")
