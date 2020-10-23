52b54d30890fdfa1711a40ea9524490b62e746a0
Kessler <ak113714@bna.com>
2020-10-23 08:44:22 -0400

    updated files for 11 days out

--- 02_analysis.R
+++ 02_analysis.R
@@ -7,7 +7,7 @@ options(scipen = 999)
 
 #set the variable for days before the election
 ### CHANGE THIS DAILY AS NEEDED
-before_election_choice <- 12
+before_election_choice <- 11
 
 
 # load processed data files from step 01
